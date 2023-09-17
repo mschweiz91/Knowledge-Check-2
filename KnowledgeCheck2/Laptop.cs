5906645ce157b90b84d037cda8b482cc6c5fe855
Mike Schweizer <maschw32836@gmail.com>
2023-09-17 14:33:42 -0400

    I think I did it!

---
+++ KnowledgeCheck2/Laptop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck2
+{
+    internal class Laptop : Computer
+    {
+        public int ScreenSizeInches { get; set; }
+
+        public Laptop(string Brand, decimal Price, int screenSizeInches)
+        {
+            this.Brand = Brand;
+            this.Price = Price;
+            this.ScreenSizeInches = screenSizeInches;
+        }
+
+        public Laptop()
+        {
+        }
+
+        public override string ToString()
+        {
+            
+            return $"This " + this.Brand
+            + " laptop has a " + this.ScreenSizeInches
+            + " inch screen and costs $" + this.Price; 
+        }
+    }
+
+    
+}
---
+++ KnowledgeCheck2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace KnowledgeCheck2
+{
+    using System;
+    using System.Collections.Generic;
+    
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many records do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+
+            var recordList = new List<Laptop>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var laptop = new Laptop();
+
+                Console.WriteLine("Adding a new laptop");
+
+                Console.WriteLine("Enter the brand name of the laptop: ");
+                laptop.Brand = Console.ReadLine();
+
+                Console.WriteLine("Enter the value for the laptop's screen size in inches: ");
+                laptop.ScreenSizeInches = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the price of the laptop: ");
+                laptop.Price = decimal.Parse(Console.ReadLine());
+
+                recordList.Add(laptop);
+
+               
+            }
+
+            // Print out the list of records using Console.WriteLine()
+
+                        
+            recordList.ForEach(recordList => Console.WriteLine(recordList));
+
+        }
+    }
+}
